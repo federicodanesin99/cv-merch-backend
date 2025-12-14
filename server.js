4705c73ac95f3d2976d56e08b12d748aa3ec2c38
federicodanesin99 <federicodanesin99@gmail.com>
2025-12-14 19:12:36 +0100

    fix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const prisma = new PrismaClient();
 // Middleware CORS configurato per dev e production
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://classe-veneta-admin.vercel.app', 'https://cv-merch-frontend.vercel.app'] // I tuoi domini production
+    ? ['https://classe-veneta-admin.vercel.app', 'https://cv-merch-frontend.vercel.app', 'https://admin-panel-cluuxuxpb-classe-venetas-projects.vercel.app'] // I tuoi domini production
     : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3001'], // Dev locale
   credentials: true
 }));
